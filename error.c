bbe6e67462ab59d3220bee2dce4b2696d8d482ec
Nelson Lopez <darltrash@icloud.com>
2024-09-17 19:25:39 -0300

    Fixed error box from appearing in terminal mode only, oops!

--- error.c
+++ error.c
@@ -60,7 +60,7 @@ void err_fatal(const char *title, const char *format, ...) {
 
     fprintf(stderr, "%s\n", message);
 
-    if (!in_terminal()) return;
+    if (in_terminal()) return;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
